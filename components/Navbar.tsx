c00a132c7c8bf6afebe3e4a3125ec5c598b73fc9
Rafael <rafaelhibiki@gmail.com>
2023-12-06 01:23:05 -0300

    feat(Setup): :wrench: Navbar Setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const session = 0;
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href={"/"}>
+          <Image src="logo.svg" alt="Flexibble" width={115} height={43} />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCemter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href={"/create-project"}>Share Work</Link>
+          </>
+        ) : (
+          <>
+            <button className="btn btn-ghost btn-small">Sign In</button>
+            <button className="btn btn-primary btn-small">Sign Up</button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
