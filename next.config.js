3afe27a83fef45124e657e2de9d201e1d1b7da8d
Rafael <rafaelhibiki@gmail.com>
2023-12-08 21:52:10 -0300

    nextConfig for vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
     images: {
         domains: ["cdn-icons-png.flaticon.com", "cloud.appwrite.io"],
         unoptimized: true
